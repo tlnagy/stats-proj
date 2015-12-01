63904eaa0c75815a22fa5d4879828c735664d884
Tamas Nagy <tamas@tamasnagy.com>
2015-11-30 22:48:42 -0800

    reformat processing to work with edgeR

--- reanalysis.R
+++ reanalysis.R
@@ -5,14 +5,27 @@ library(ggplot2)
 
 raw_data <- read_excel("data/nature13990-s4.xlsx", sheet = "Raw shRNA counts")
 
-cpms <- edgeR::cpm(raw_data[,-(1:3)])
-log_cpms <- edgeR::cpm(raw_data[,-(1:3)], log = TRUE, prior.count = 0.5)
-rownames(cpms) <- raw_data$`Target Gene Symbol`
-rownames(log_cpms) <- raw_data$`Target Gene Symbol`
+# experiment design info
+exp.info <- matrix(unlist(strsplit(colnames(raw_data[,-(1:3)]), "_")), ncol=4, byrow = TRUE)
+cellstage <- factor(exp.info[,1])
+treatment <- factor(exp.info[, 3])
+treatment <- relevel(treatment, ref = "24h")
+timepoints <- factor(exp.info[,4])
+
+groups <- factor(paste(cellstage, treatment, sep="."))
+data <- DGEList(counts = raw_data[,-(1:3)], genes = raw_data[,1:3], group = groups)
 
 # Select only hairpins with CPM > 0.5 in more than two samples
-cpm_mask <- rowSums(cpms > 0.5) >= 2
-cpms <- cpms[cpm_mask, ]
+shrnas.to.keep <- rowSums(edgeR::cpm(data) > 0.5) >= 2
+data <- data[shrnas.to.keep, , keep.lib.sizes=FALSE]
 
-log_cors <- cor(log_cpms)
-heatmap(log_cors, Rowv = NA, Colv = NA, scale = "none")
+data <- edgeR::calcNormFactors(data)
+
+# construct design matrix
+design <- model.matrix(~treatment + treatment:cellstage)
+rownames(design) <- colnames(data)
+
+# estimate common, tagwise, and trend dispersion
+data <- edgeR::estimateDisp(data, design, robust=TRUE)
+plotBCV(data)
+
