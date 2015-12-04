9e29e5a9393183494707bdfee9bf6496a990f73a
Tamas Nagy <tamas@tamasnagy.com>
2015-12-04 15:13:57 -0800

    implement most of the code needed for fig 3b

--- reanalysis.R
+++ reanalysis.R
@@ -39,11 +39,48 @@ log.cpms <- log.cpms[ ,rev(col.order)]
 heatmap(cor(log.cpms), Rowv = NA, Colv = NA, scale = "none")
 
 # construct design matrix
-design <- model.matrix(~treatment + treatment:cellstage)
-rownames(design) <- colnames(data)
+groups <- factor(paste(cellstage, treatment, sep="."))
+design <- model.matrix(~0+groups)
+colnames(design) <- levels(groups)
 
 # estimate common, tagwise, and trend dispersion and plot the biological
 # coefficient of variation (BCV)
 data <- edgeR::estimateDisp(data, design, robust=TRUE)
 plotBCV(data)
 
+# construct contrasts (aka comparison vectors)
+contrasts <- makeContrasts(ERGvsControl = ERG.24h-ERG.plus,
+                           ERGvsMinus = ERG.minus - ERG.plus,
+                           MRGvsControl = MRG.24h-MRG.plus,
+                           MRGvsMinus = MRG.minus - MRG.plus,
+                           NEvsControl = NE.24h-NE.plus,
+                           NEvsMinus = NE.minus - NE.plus, levels=design2)
+
+fit <- edgeR::glmFit(data, design = design)
+result <- list()
+
+comparisons <- colnames(contrasts)
+for (val in comparisons){
+  lrt <- edgeR::glmLRT(fit, contrast = contrasts[, val])
+  sig.genes <- data.frame(topTags(lrt, n=100000, p.value = 0.05))
+
+  by.gene <- group_by(sig.genes, Target.Gene.Symbol)
+  same.as.majority.sign <- function(x) as.numeric(sign(x) == sign(mean(sign(x))))
+  gene.info <- summarize(by.gene, count = n(), means = mean(sign(logFC)),
+                         wmeanfc = sum(logFC*logCPM*same.as.majority.sign(logFC)/sum(logCPM*same.as.majority.sign(logFC))))
+  gene.info <- filter(gene.info, count >= 2 & means != 0)
+  gene.info <- gene.info[c("Target.Gene.Symbol", "wmeanfc")]
+  result[[val]] <- gene.info
+}
+
+tophits <- list()
+
+# The PlusvsControl and PlusvsMinus samples for each stage by taking the
+# maximum abs value in either as the score on a gene by gene basis
+for (i in seq(1, length(comparisons), 2)){
+  combined <- rbind(result[[comparisons[i]]], result[[comparisons[i+1]]])
+  abs.ordered <- combined[order(combined$Target.Gene.Symbol,
+                                -abs(combined$wmeanfc)), ]
+  take.max.abs <- abs.ordered[!duplicated(abs.ordered$Target.Gene.Symbol), ]
+  tophits[[strsplit(comparisons[i], "vs")[[1]][1]]] <- take.max.abs
+}
